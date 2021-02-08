c06dc10026c49441aa6e58daa64ecb0d6c06d119
Ewa Grązka <fk.grazka@gmail.com>
2021-02-08 18:22:37 +0100

    results page (structure, css, finder)

--- src/js/Finder.js
+++ src/js/Finder.js
@@ -1,49 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "../scss/Finder.scss";
-import { useHistory } from "react-router";
 
-function Finder() {
-  const [photo, setPhoto] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-  const [results, setResults] = useState(false);
-  const history = useHistory();
-  const handleSubmit = (e, props) => {
+function Finder(props) {
+  const { results, change } = props;
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    fetch(
-      "https://api.unsplash.com/search/photos?page=5&per_page=20&query=" +
-        photo +
-        "&client_id=" +
-        "jDbtQg1MmInd9TCKgJBQDwREA4fpVrRKhomN4ekVhBs",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept-Version": "v1",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          // setIsLoaded(true);
-          // setItems(result);
-          console.log(result, "result");
-          setResults(result.results);
-        },
-
-        (error) => {
-          // setIsLoaded(true);
-          // setError(error);
-          console.log(error, "errors");
-        }
-      );
-    history.push({
-      pathname: "/result",
-    });
-  };
-  const handleChange = (e) => {
-    setPhoto(e.target.value);
+    results();
   };
 
   return (
@@ -52,28 +14,13 @@ function Finder() {
         <div className="searchInputWrapper">
           <i className="fas fa-search"></i>
           <input
-            onChange={handleChange}
+            onChange={(e) => change(e)}
             type="search"
             name="photo"
             placeholder="Search free high-resolution photos"
           />
         </div>
       </form>
-      <div className="photosWrapper">
-        <div className="photos">
-          {results
-            ? results.map((photo) => (
-                <div class="photoWrapper">
-                  <img
-                    className="photo"
-                    key={photo.id}
-                    src={photo.urls.regular}
-                  ></img>
-                </div>
-              ))
-            : null}
-        </div>
-      </div>
     </>
   );
 }
--- src/js/MainPage.js
+++ src/js/MainPage.js
@@ -1,19 +1,18 @@
 import React from "react";
 import Finder from "./Finder";
 import "../scss/MainPage.scss";
+import Result from "./Result";
 
-function MainPage() {
+function MainPage(props) {
   return (
     <div className="mainWrapper">
-      <div>
-        <div className="titleWrapper">
-          <h1>Unsplash</h1>
-          <h3>The internet source of freely-usable images.</h3>
-          <h3>Powered by creators everywhere.</h3>
-        </div>
-        <Finder />
-        <h5>Trending: flower, wallpapers, backgrounds, happy, love</h5>
+      <div className="titleWrapper">
+        <h1>Unsplash</h1>
+        <h3>The internet source of freely-usable images.</h3>
+        <h3>Powered by creators everywhere.</h3>
       </div>
+      <Finder results={props.results} change={props.change} />
+      <h5>Trending: flower, wallpapers, backgrounds, happy, love</h5>
     </div>
   );
 }
